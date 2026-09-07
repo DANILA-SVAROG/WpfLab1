fcd360a72ecfcd22b9dbea16e24477571b8f2a6e
admin <admin@DESKTOP-FH03AMJ>
2026-09-07 19:45:03 +0300

    Добавлен класс Point2D

---
+++ WpfLab1/Point2D.cs
@@ -0,0 +1,27 @@
+﻿namespace WpfLab1
+{
+    public class Point2D
+    {
+        //Свойства класса — чтение снаружи, изменение только изнутри класса
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        //Конструктор класса
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        //Методы для изменения координат (смещение на заданную величину)
+        public void AddX(int x)
+        {
+            X += x;
+        }
+
+        public void AddY(int y)
+        {
+            Y += y;
+        }
+    }
+}
